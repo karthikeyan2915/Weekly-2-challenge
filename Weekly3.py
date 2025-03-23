7e1aa83b563ee8b51b9a202c2e52ca97f63107da
karthikeyan2915 <k.karthikeyan2915@gmail.com>
2025-03-23 19:48:12 +0530

    Add files via upload

---
+++ Weekly3.py
@@ -0,0 +1,26 @@
+def roman (s: str) :
+    roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    total = 0
+    prev_value = 0
+    
+    for char in reversed(s):
+        value = roman_values[char]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+            prev_value = value
+
+    return total
+
+a = input("Enter the Roman numeral: ")
+print("Integer value:", roman(a.upper()))
